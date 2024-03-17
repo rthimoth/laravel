ea66d168f0fe7f6dbc852181b8de68b11d01a421
rthimoth <thimothee.ranvin@hotmail.com>
2024-03-17 19:29:20 +0100

    update data base

--- database/migrations/2021_06_07_082644_create_sessions_table.php
+++ database/migrations/2021_06_07_082644_create_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class phpCreateSessionsTable extends Migration
+class CreateSessionsTable extends Migration
 {
     /**
      * Run the migrations.
