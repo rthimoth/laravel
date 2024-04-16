984f125c94736a9c03e5b42eb772b3766302173f
cpenot <clem.cmp@hotmail.com>
2024-04-16 12:45:13 +0200

    sidebar padding

--- resources/views/components/partials/sidenav.blade.php
+++ resources/views/components/partials/sidenav.blade.php
@@ -7,17 +7,15 @@
         px-2">
 
     {{-- Categories --}}
-    <div class="w-full pl-4 p-6 pt-4 rounded-2xl bg-custom flex flex-col space-y-4">
-        <h2 class="font-bold text-sm text-white">Categories</h2>
+    <div class="w-full py-4  rounded-2xl bg-custom flex flex-col space-y-4">
+        <h2 class=" pl-1 font-bold text-sm text-white">Categories</h2>
 
         <ul class="w-full 
-            flex flex-col">
-            <li>
-                <a href="#" class="optionSidebar">
-                    <p>Category One</p>
-                    <span class="px-2 text-white bg-green-300 rounded">45</span>
-                </a>
-            </li>
+            flex flex-col ">
+            <a href="#" class="optionSidebar">
+                <p>Category One</p>
+                <span class="px-2 text-white bg-green-300 rounded">45</span>
+            </a>
         </ul>
     </div>
 
