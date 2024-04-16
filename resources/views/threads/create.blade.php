1aaab80366bf304ac8b1a4429387041df9c90777
cpenot <clem.cmp@hotmail.com>
2024-04-16 13:07:09 +0200

    create post finished

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -6,27 +6,22 @@
         <section class="flex flex-col col-span-3 gap-y-4">
             <small class="text-sm text-gray-400">discussion>create</small>
 
-            <article class="p-5 bg-white shadow">
-                <div class="grid grid-cols-8">
-
-                    {{-- Avatar --}}
-                    <div class="col-span-1">
-                        <x-user.avatar />
-                    </div>
+            <article class="p-5 ">
+                <div class="grid grid-cols-8 ">
 
                     {{-- Create --}}
                     <div class="col-span-7 space-y-6">
                         <x-form action="#">
                             <div class="space-y-8">
                                 {{-- Title --}}
-                                <div>
+                                <div class=" w-full h-auto bg-custom p-2 rounded-lg ">
                                     <x-form.label for="title" value="{{ __('Title') }}" />
                                     <x-form.input id="title" class="block w-full mt-1" type="text" name="title" :value="old('title')" required autofocus />
                                     <x-form.error for="title" />
                                 </div>
 
                                 {{-- Category --}}
-                                <div>
+                                <div class="w-full h-auto bg-custom p-2 rounded-lg">
                                     <x-form.label for="category" value="{{ __('Category') }}" />
                                     <select name="category" id="category" class="w-full text-sm border-gray-300 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
                                         <option value="">Select Category</option>
@@ -39,9 +34,10 @@
                                 </div>
 
                                 {{-- Body --}}
-                                <div>
+                                
+                                <div class="w-full h-auto bg-custom p-2 rounded-lg">
                                     <x-form.label for="body" value="{{ __('Description') }}" />
-                                    <x-trix name="about" styling="shadow-inner bg-gray-100" />
+                                    <x-trix name="about" class="bg-white p-2 rounded-lg" styling="shadow-inner bg-white" />
                                 </div>
 
                                 {{-- Button --}}
