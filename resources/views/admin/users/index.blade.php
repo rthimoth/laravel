8eeb70fadc70dfc3bb8b28c4df19a9f7a7d5e453
cpenot <clem.cmp@hotmail.com>
2024-05-10 16:52:22 +0200

    admin

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -20,28 +20,6 @@
                     </tr>
                 </thead>
 
-                <tbody class="divide-y divide-gray-200 divide-solid">
-                    <tr>
-                        <x-table.data>
-                            <div>John Doe</div>
-                        </x-table.data>
-                        <x-table.data>
-                            <div>Some description of bio....</div>
-                        </x-table.data>
-                        <x-table.data>
-                            <div>date</div>
-                        </x-table.data>
-                        <x-table.data>
-                            <div class="px-2 py-1 text-center text-gray-700 bg-green-200 rounded">
-                                Moderator
-                            </div>
-                        </x-table.data>
-                        <x-table.data>
-                            <div class="text-center">2005-14-06</div>
-                        </x-table.data>
-                    </tr>
-                </tbody>
-
             </table>
         </div>
     </section>
